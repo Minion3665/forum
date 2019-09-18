ef04d3b4d4463795198b50d6f84141127bcb1506
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-18 19:00:24 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DisplayBoards from "boards/displayBoards.js";
 
 let buildNumber = 0.25;
 
@@ -13,27 +14,7 @@ console.log(
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route
-            path="/status"
-            render={() => {
-              changeFavicon('/images/canaryLogo.png');
-              return (
-                <iframe src="https://chromebook777production.statuspage.io/" />
-              );
-            }}
-          />
-          <Route
-            render={() => {
-              changeFavicon('/images/chromebotLogo.png');
-              return (
-                <iframe src="https://chromebot-canary-and-chromebot.weebly.com/" />
-              );
-            }}
-          />
-        </Switch>
-      </Router>
+      <DisplayBoards/>
     );
   }
 }
