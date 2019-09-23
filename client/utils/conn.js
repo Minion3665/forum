4e287c1c39a42fe5679efae164e1430821361d3b
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-23 20:32:08 +0100

    Update conn.js

--- client/utils/conn.js
+++ client/utils/conn.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+const cookie = new Cookies();
 
 const axiosGitHubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
 });
 
+cookie.get("gh-token")
+cookie.set("gh-token", result.token, { path: "/", maxAge: 604800000 });
+cookie.remove("gh-token")
