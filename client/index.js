28619450082d761fcea26c4c84a0e5c16e9f97b0
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-26 20:40:27 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route component={BoardPosts}/>
+        <Route exact path="/" component={BoardPosts}/>
         <DisplayBoards/>
       </Router>
     );
