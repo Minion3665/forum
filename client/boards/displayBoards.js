aa106afbcae88eb24029dc4011b0776a35065b6a
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-26 15:40:25 +0100

    Update displayBoards.js

--- client/boards/displayBoards.js
+++ client/boards/displayBoards.js
@@ -89,6 +89,8 @@ class DisplayBoards extends Component {
 					<Board name="off topic" description="all the posts that don't fit in the original boards they were posted in" bgColor="#ffffff" />
 					{
 						Object.keys(boards).map((board) => {
+							console.log(board)
+							console.log(board.name)
 							return (
 								<Board name={board.name} description={board.description} bgColor={board.color} />
 							); // Create an instance of Board for each board in the forum
