3de7c944d65fd6648eb1b8e6081a1dddc625ded4
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-26 15:56:29 +0100

    Update displayBoards.js

--- client/boards/displayBoards.js
+++ client/boards/displayBoards.js
@@ -89,6 +89,7 @@ class DisplayBoards extends Component {
 					<Board name="off topic" description="all the posts that don't fit in the original boards they were posted in" bgColor="#ffffff" />
 					{
 						Object.keys(boards).map((id, board) => {
+							console.log(id)
 							console.log(board)
 							console.log(board.name)
 							return (
