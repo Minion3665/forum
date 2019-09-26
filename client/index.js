74f5ffdadd131d338ae6176b672950ab6302196f
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-26 18:20:01 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DisplayBoards from "./boards/displayBoards.js";
+import BoardPosts from "./boards/displayBoards.js";
+//import UserPosts from "./boards/displayBoards.js";
 
 let buildNumber = 0.26;
 
@@ -15,6 +17,10 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Switch>
+          <Route path="/board/:board" component={BoardPosts}/>
+          //<Route path="/user/:user" component={User}/>
+        </Switch>
         <DisplayBoards/>
       </Router>
     );
