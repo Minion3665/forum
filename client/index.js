04bdc6e788935945204b3da95f888245be4a2f47
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-26 18:20:17 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DisplayBoards from "./boards/displayBoards.js";
-import BoardPosts from "./boards/displayBoards.js";
+import BoardPosts from "./posts/boards.js";
 //import UserPosts from "./boards/displayBoards.js";
 
 let buildNumber = 0.26;
