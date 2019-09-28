eb5460b09fe47d1f561ef9096a5238108c3e2d77
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-09-28 08:49:30 +0100

    Update conn.js

--- client/utils/conn.js
+++ client/utils/conn.js
@@ -36,19 +36,11 @@ function logout() {
 }
 
 function getLabels() { // Get all labels in the forum
-	axiosGitHubGraphQL
-		.post('', { query: GET_LABELS })
-		.then(result => console.log(result));
-  return [
-    {name: "Board:General", description: "just general posts", color: "#aa0000"},
-    {name: "Board:Casual-Coding", description: "but also casual coding yes", color: "#aa0000"},
-    {name: "Board:Casual Coding", description: "just general posts", color: "#9000ff"},
-    {name: "Board:Coding Help", description: "just general posts", color: "#18d600"},
-    {name: "Board:Minion3665's Testing", description: "just general posts", color: "#aa0000"},
-    {name: "Board:Random", description: "just general posts", color: "#ffff00"},
-    {name: "Moderation:OffTopic", description: "just general posts", color: "#1affff"},
-    {name: "just a random pointless label to screw stuff up", description: "just general posts", color: "#aa0000"},
-  ]; // This returns a fake result for now, as I can't be bothered to do graphql until later
+	return axiosGitHubGraphQL
+	.post('', { query: GET_LABELS })
+	.then((result) => {
+		return result.data.data.repository.edges;
+	});
 }
 
 export { getLabels };
