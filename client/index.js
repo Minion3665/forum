f1bde5dc4034c1335b4712d3e9286e8d6ad301ec
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-02 17:21:39 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -43,7 +43,8 @@ class Authentication extends Component {
 			client_secret: "65209c048eca0255f770c84de322ba0a9d59055f",
 			code: ghCode
 		}).then((response) => {
-			let res = querySearch(response.data);
+			//let res = querySearch(response.data);
+			let res = response.data;
 			console.log(res);
 			if (res.error) {
 				console.log(res.error);
