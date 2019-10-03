9f818ea29883aa0abdd14f114401594bb065e8f3
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2019-10-03 15:38:33 +0100

    Update userinput.js

--- client/utils/userinput.js
+++ client/utils/userinput.js
@@ -28,6 +28,7 @@ class UserInput extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			bodyValue: "",
 			type: (props.type) ? props.type : "post",
 			verb: (props.type != "comment") ? "to" : "on",
 			to: (props.to) ? props.to : "this",
@@ -35,6 +36,15 @@ class UserInput extends Component {
 			width: (props.type != "comment") ? "85vw" : "75vw",
 			transform: (props.type != "comment") ? "translate(-50%, 0)" : "translate(-45%, 0)",
 		}
+		this.handleBodyChange = this.handleBodyChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleBodyChange(event) {
+		this.setState({bodyValue: event.target.value});
+	}
+	handleSubmit(event) {
+		onsubmit("A "+this.state.type, this.state.bodyValue);
+		event.preventDefault();
 	}
 	render() {
 		baseInputContainerStyle.width = this.state.width;
@@ -42,9 +52,9 @@ class UserInput extends Component {
 		return (
 			<React.Fragment>
 				<div style={baseInputContainerStyle}>
-					<form onsubmit={this.state.onsubmit}>
+					<form onsubmit={this.handleSubmit}>
 						<span>{uppercaseFirstLetter(this.state.type)} {this.state.verb} {this.state.to.toLowerCase()}</span>
-						<textarea style={baseTextAreaStyle}/>
+						<textarea value={this.state.bodyValue} onChange={this.handleBodyChange} style={baseTextAreaStyle}/>
 						<button type="reset">Clear</button>{" "}
 						<button type="submit">{uppercaseFirstLetter(this.state.type)}!</button>
 					</form>
