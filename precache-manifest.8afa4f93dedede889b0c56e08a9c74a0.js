b2f6dd47fbc37a5b24ee888497e3bcd98da197ff
Minion3665 <34243578+Minion3665@users.noreply.github.com>
2020-01-06 11:53:51 +0000

    Deploying to gh-pages from master 2d0c22329c076f70575d49292349239b1f1f1134

---
+++ precache-manifest.8afa4f93dedede889b0c56e08a9c74a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d24f737831b7b96270c5a4def2598e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ba549eee3c64da3cad9",
+    "url": "/static/js/2.b6e22ae7.chunk.js"
+  },
+  {
+    "revision": "f1ba7ffe90e37988d6dc5912be7f30cb",
+    "url": "/static/js/2.b6e22ae7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4d6358ec3ccaa053eed",
+    "url": "/static/js/main.9a757c6f.chunk.js"
+  },
+  {
+    "revision": "18bf695c24fd2406d273",
+    "url": "/static/js/runtime-main.64fadb52.js"
+  }
+]);
